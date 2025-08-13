a0204a31b6c65e7286ea0395380567818740e46c
peaceobiefule <pobiefule@byupathway.edu>
2025-08-13 02:03:19 +0100

    NEW

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,98 @@
 using System;
+using System.Collections.Generic;
+
+abstract class Activity
+{
+    private DateTime _date;
+    private int _minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public DateTime Date => _date;
+    public int Minutes => _minutes;
+
+    public abstract double GetDistance(); // in km
+    public abstract double GetSpeed();    // in kph
+    public abstract double GetPace();     // in min per km
+
+    public virtual string GetSummary()
+    {
+        return $"{_date:dd MMM yyyy} {GetType().Name} ({_minutes} min) - " +
+               $"Distance: {GetDistance():0.0} km, " +
+               $"Speed: {GetSpeed():0.0} kph, " +
+               $"Pace: {GetPace():0.00} min per km";
+    }
+}
+
+class Running : Activity
+{
+    private double _distance; // in km
+
+    public Running(DateTime date, int minutes, double distance)
+        : base(date, minutes)
+    {
+        _distance = distance;
+    }
+
+    public override double GetDistance() => _distance;
+
+    public override double GetSpeed() => (GetDistance() / Minutes) * 60;
+
+    public override double GetPace() => Minutes / GetDistance();
+}
+
+class Cycling : Activity
+{
+    private double _speed; // in kph
+
+    public Cycling(DateTime date, int minutes, double speed)
+        : base(date, minutes)
+    {
+        _speed = speed;
+    }
+
+    public override double GetDistance() => (_speed * Minutes) / 60;
+
+    public override double GetSpeed() => _speed;
+
+    public override double GetPace() => 60 / _speed;
+}
+
+class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(DateTime date, int minutes, int laps)
+        : base(date, minutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance() => _laps * 50 / 1000.0;
+
+    public override double GetSpeed() => (GetDistance() / Minutes) * 60;
+
+    public override double GetPace() => Minutes / GetDistance();
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2022, 11, 3), 30, 4.8),
+            new Cycling(new DateTime(2022, 11, 3), 45, 20.0),
+            new Swimming(new DateTime(2022, 11, 3), 30, 20)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
